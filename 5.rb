35ca26bc58de4b04b84f52fd1219671222c5e8c7
Jose <jimarb@outlook.com>
2019-06-21 01:25:33 -0400

    Exercise 5 finished

---
+++ 5.rb
@@ -0,0 +1,24 @@
+module Herviboro
+    Definir = 'Sólo me alimento de vegetales!'
+    def self.definir
+        Definir
+    end
+    def dieta
+        "Soy un Herviboro!"
+    end
+end
+class Animal
+    def saludar
+        "Soy un animal!"
+    end
+end
+class Conejo < Animal 
+    include Herviboro
+    def initialize(name)
+        @name = name
+    end
+end
+conejo = Conejo.new('Bugs Bunny')
+conejo.saludar
+puts conejo.dieta
+Herviboro.definir
